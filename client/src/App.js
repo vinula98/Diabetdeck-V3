d431c375bbd235c3b3e5b755a5b8e6520ec04025
vinulak <vinulakas.98@gmail.com>
2022-05-01 20:57:56 +0530

    UI added

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,44 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import Chatbot from "react-chatbot-kit";
+import 'react-chatbot-kit/build/main.css';
+
+import config from "./chatbot/config";
+import MessageParser from "./chatbot/MessageParser";
+import ActionProvider from "./chatbot/ActionProvider";
 
 function App() {
 
-  const [data, setData] = useState([{}])
+  // const [data, setData] = useState([{}])
 
-  useEffect(() => {
-    fetch("/members").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
+  // useEffect(() => {
+  //   fetch("/members").then(
+  //     res => res.json()
+  //   ).then(
+  //     data => {
+  //       setData(data)
+  //       console.log(data)
+  //     }
+  //   )
+  // }, [])
+
   return (
-    <div>
+    <div className="App">
+      <Chatbot
+        config={config}
+        messageParser={MessageParser}
+        actionProvider={ActionProvider}
+      />
 
-      {(typeof data.members === 'undefined') ? (
+      {/* {(typeof data.members === 'undefined') ? (
         <p>Loading...</p>
       ) : (
         data.members.map((member, i) => (
           <p key={i}>{member}</p>
         ))
-      )}
-      
+      )} */}
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
